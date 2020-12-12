298db1a846b7bf60ddc5d5c610e22f42d1a06727
zhangbiwu <zhangbiwu@yy.com>
2020-12-12 18:49:45 +0800

    koa2-demo

---
+++ nodejs/koa2-note/demo/app.js
@@ -0,0 +1,47 @@
+const Koa = require('koa');
+const Router = require('koa-router')
+const app = new Koa();
+
+// 子路由1
+let home = new Router()
+home.get('/', async ( ctx )=>{
+  let html = 'Hello, koa2'
+  ctx.body = html
+})
+
+// 子路由2
+let page = new Router()
+page.get('/404', async ( ctx )=>{
+  ctx.body = '404 page!'
+}).get('/helloworld', async ( ctx )=>{
+  ctx.body = 'helloworld page!'
+})
+
+app.use(home.routes());
+app.use(page.routes());
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+
+app.listen(3000);
