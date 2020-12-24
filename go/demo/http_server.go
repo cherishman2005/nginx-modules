c418ba10f877f08bf59b769ca445d30fc41578b8
zhangbiwu <zhangbiwu@yy.com>
2020-12-24 16:30:47 +0800

    update

--- go/demo/http_server.go
+++ go/demo/http_server.go
@@ -4,12 +4,30 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"encoding/json"
 )
 
-func timeHandler(format string) http.Handler {
+type Person struct {
+    Name string `json:"name"`
+    Age  int    `json:"age"`
+    Addr string `json:"addr,omitempty"`
+}
+
+func userHandler(format string) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		tm := time.Now().Format(format)
-		w.Write([]byte("The time is: " + tm))
+		//tm := time.Now().Format(format)
+
+		p1 := Person{
+			Name: "awu",
+			Age:  18,
+		}
+
+		data, err := json.Marshal(p1)
+		if err != nil {
+			panic(err)
+		}
+
+		w.Write([]byte(data))
 	}
 	return http.HandlerFunc(fn)
 }
@@ -17,8 +35,8 @@ func timeHandler(format string) http.Handler {
 func main() {
 	mux := http.NewServeMux()
 
-	th := timeHandler(time.RFC1123)
-	mux.Handle("/time", th)
+	th := userHandler(time.RFC1123)
+	mux.Handle("/user", th)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", mux)
