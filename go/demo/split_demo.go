cf60b56cf9a4e9e7d425a31798d2e53ac35eb2a6
zhangbiwu <zhangbiwu@yy.com>
2021-01-08 11:49:38 +0800

    update

--- go/demo/split_demo.go
+++ go/demo/split_demo.go
@@ -1,15 +1,35 @@
 package main
- 
+
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
- 
+
+var (
+	WhitelistUidMap map[uint64]bool
+)
+
+func init() {
+	WhitelistUidMap = make(map[uint64]bool)
+}
+
 func main() {
-	demo := "I&love&Go,&and&I&also&love&Python."
-	string_slice := strings.Split(demo, "&")
- 
-	fmt.Println("result:",string_slice)
-	fmt.Println("len:",len(string_slice))
-	fmt.Println("cap:", cap(string_slice))
+	s := "123,456,789"
+	
+	TransferStringToMap(s, &WhitelistUidMap)
+	
+	fmt.Println(WhitelistUidMap)
+}
+
+func TransferStringToMap(s string, slice *map[uint64]bool) {
+	elements := strings.Split(s, ",")
+	
+	for _, v := range elements {
+		uid, err := strconv.ParseUint(v, 10, 64);
+		if err != nil {
+			continue
+		}
+		(*slice)[uid] = true
+	}
 }
