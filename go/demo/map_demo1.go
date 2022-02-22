7f34490678f595c5c1a0e2c2a4a8a79035958235
zhangbiwu <cherishman2005@163.com>
2022-02-22 21:13:53 +0800

    Add files via upload

---
+++ go/demo/map_demo1.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+    "fmt"
+)
+
+type RouteKey struct {
+	Appid uint32
+	BusinessAuth uint32
+}
+
+type AuthConfig struct {
+	Appid uint32
+	BusinessAuth uint32
+	Srvname string
+	Pass uint32
+}
+
+
+
+
+func main() {
+    configs := make(map[*RouteKey]*AuthConfig)
+    
+    key := &RouteKey{
+        Appid: 22014,
+        BusinessAuth: 16777218,
+    }
+    config := &AuthConfig{
+        Appid: 22014,
+        BusinessAuth: 16777218,
+        Srvname: "auth_audio_route",
+        Pass: 0,
+    }
+    configs[key] = config
+    
+    
+    
+    key1 := &RouteKey{
+        Appid: 15013,
+        BusinessAuth: 16777218,
+    }
+    config1 := &AuthConfig{
+        Appid: 15013,
+        BusinessAuth: 16777218,
+        Srvname: "auth_audio_route",
+        Pass: 0,
+    }
+    configs[key1] = config1
+    
+    fmt.Println("configs:", configs)
+    
+    if v, ok := configs[key]; ok {
+        fmt.Println(v)
+    } else {
+        fmt.Println("not found")
+    }
+    
+    
+    key2 := &RouteKey{
+        Appid: 15013,
+        BusinessAuth: 16777218,
+    }
+    if v, ok := configs[key2]; ok {
+        fmt.Println(v)
+    } else {
+        fmt.Println("not found")
+    }
+}
+
+/*
+    运行结果：
+    configs: map[0xc0000140b8:0xc00000c060 0xc0000140d0:0xc00000c080]
+    &{22014 16777218 auth_audio_route 0}
+    not found
+*/
