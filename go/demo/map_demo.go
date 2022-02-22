3891c1e33d4bc15f485ad85a24d3c38293137db3
zhangbiwu <cherishman2005@163.com>
2022-02-22 21:17:38 +0800

    Add files via upload

--- go/demo/map_demo.go
+++ go/demo/map_demo.go
@@ -26,15 +26,29 @@ func main() {
         Appid: 22014,
         BusinessAuth: 16777218,
     }
-
     config := &AuthConfig{
         Appid: 22014,
         BusinessAuth: 16777218,
         Srvname: "auth_audio_route",
         Pass: 0,
     }
-    
     configs[key] = config
+    
+    
+    
+    key1 := RouteKey{
+        Appid: 15013,
+        BusinessAuth: 16777218,
+    }
+    config1 := &AuthConfig{
+        Appid: 15013,
+        BusinessAuth: 16777218,
+        Srvname: "auth_audio_route",
+        Pass: 0,
+    }
+    configs[key1] = config1
+    
+    fmt.Println("configs:", configs)
     
     if v, ok := configs[key]; ok {
         fmt.Println(v)
@@ -43,13 +57,20 @@ func main() {
     }
     
     
-    key1 := RouteKey{
+    key2 := RouteKey{
         Appid: 15013,
         BusinessAuth: 16777218,
     }
-    if v, ok := configs[key1]; ok {
+    if v, ok := configs[key2]; ok {
         fmt.Println(v)
     } else {
         fmt.Println("not found")
     }
 }
+
+/*
+    运行结果：
+    configs: map[{15013 16777218}:0xc00000c080 {22014 16777218}:0xc00000c060]
+    &{22014 16777218 auth_audio_route 0}
+    &{15013 16777218 auth_audio_route 0}
+*/
