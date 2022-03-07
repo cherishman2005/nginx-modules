4fe9ecc3cd31bac1b838baa9260f68bbf724c99b
zhangbiwu <cherishman2005@163.com>
2022-03-07 16:29:53 +0800

    Add files via upload

---
+++ nginx/make_auto.sh
@@ -0,0 +1,9 @@
+./configure --prefix=/data/services/nginx/openresty --with-debug --with-http_v2_module --with-stream \
+            --with-cc-opt="-g -O2" \
+            --with-openssl="../openssl-1.0.2k"
+ 
+
+make -j40 &&  make install
+
+exit 0
+
