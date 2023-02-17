6e12b33a04a10fb7dc67f0159fe1197bcd96359b
zhangbiwu <cherishman2005@163.com>
2023-02-18 01:23:07 +0800

    Update config.go

--- go/demo/config/config.go
+++ go/demo/config/config.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"gopkg.in/yaml.v3"
+    "fmt"
+    "log"
+    "os"
+    "gopkg.in/yaml.v3"
 )
 
 type ConfigBasic struct {
-	HttpPort       int  `yaml:"http_port"`// listen port for http
+    HttpPort       int  `yaml:"http_port"`// listen port for http
 }
 
 type ApiConfig struct {
-	// basic server config
-	Server ConfigBasic  `yaml:"server"`
+    // basic server config
+    Server ConfigBasic  `yaml:"server"`
 }
 
 var data = `
@@ -22,12 +22,39 @@ server:
     http_port: 8080
 `
 
-func main() {
-	var config ApiConfig
+func ReadManifest() (*ApiConfig, error) {
+    p := "./api.yml"
 
-	err := yaml.Unmarshal([]byte(data), &config)
-	if err != nil {
-		log.Fatalf("cannot unmarshal data: %v", err)
-	}
-	fmt.Println(config.Server.HttpPort)
+    file, err := os.Open(p)
+    if err != nil {
+        return nil, fmt.Errorf("failed to open the plugin manifest %s: %w", p, err)
+    }
+
+    defer func() { _ = file.Close() }()
+
+    m := &ApiConfig{}
+    err = yaml.NewDecoder(file).Decode(m)
+    if err != nil {
+        return nil, fmt.Errorf("failed to decode the plugin manifest %s: %w", p, err)
+    }
+
+    return m, nil
+}
+
+func main() {
+    var config ApiConfig
+
+    err := yaml.Unmarshal([]byte(data), &config)
+    if err != nil {
+        log.Fatalf("cannot unmarshal data: %v", err)
+    }
+    fmt.Println(config.Server.HttpPort)
+    
+
+    config1, err = ReadManifest()
+    if err != nil {
+        log.Fatalf("cannot unmarshal data: %v", err)
+    }
+    fmt.Println(config1.Server.HttpPort)
+    
 }
