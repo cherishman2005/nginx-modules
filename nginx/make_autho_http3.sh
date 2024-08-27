3bfdf5a7f260f0105bc056c56aa4d6414ab85fe5
zhangbiwu <cherishman2005@163.com>
2024-08-27 11:01:11 +0800

    Update make_autho_http3.sh

--- nginx/make_autho_http3.sh
+++ nginx/make_autho_http3.sh
@@ -1,6 +1,6 @@
 ./configure --prefix=/data/services/nginx/openresty --with-debug --with-http_v2_module --with-http_v3_module --with-stream \
             --with-cc-opt="-g -O2" \
-            --with-openssl="../openssl-1.0.2k"
+            --with-openssl="../openssl-3.3.1"
  
 
 make -j2 &&  make install
