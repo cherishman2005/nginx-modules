54cbe1cd7ff8877f710a4b80eeceed51b9f58b68
zhangbiwu <cherishman2005@163.com>
2024-09-14 17:43:44 +0800

    Update make_auto.sh

--- nginx/make_auto.sh
+++ nginx/make_auto.sh
@@ -1,4 +1,5 @@
 ./configure --prefix=/data/services/nginx/openresty --with-debug --with-http_v2_module --with-stream \
+            --with-http_stub_status_module \
             --with-cc-opt="-g -O2" \
             --with-openssl="../openssl-1.0.2k"
  
