cef91ab34fb93bc9691fe6c52c1615addf0aaa10
zhangbiwu <cherishman2005@163.com>
2025-05-03 17:52:41 +0800

    Create sync_map_demo.go

---
+++ go/advanced/sync_map_demo.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var m sync.Map
+
+	// 存储键值对
+	m.Store("apple", 1)
+	m.Store("banana", 2)
+	m.Store("cherry", 3)
+
+	// 加载键值对
+	if value, ok := m.Load("apple"); ok {
+		fmt.Println("Value of apple:", value)
+	}
+
+	// 加载或存储
+	actual, loaded := m.LoadOrStore("apple", 4)
+	fmt.Println("Actual value of apple:", actual)
+	fmt.Println("Was apple already loaded?", loaded)
+
+	// 删除键值对
+	m.Delete("banana")
+
+	// 遍历键值对
+	m.Range(func(key, value interface{}) bool {
+		fmt.Printf("Key: %s, Value: %d\n", key, value)
+		return true
+	})
+}
