7ce019e769a08a56fa21e974a836a0014a2eccc2
zhangbiwu <cherishman2005@163.com>
2025-06-05 22:54:17 +0800

    Create enum.rs

---
+++ rust/demo/enum.rs
@@ -0,0 +1,17 @@
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+
+fn main() {
+    let dire = Direction::South;
+    match dire {
+        Direction::East => println!("East"),
+        Direction::North | Direction::South => {
+            println!("South or North");
+        },
+        _ => println!("West"),
+    };
+}
